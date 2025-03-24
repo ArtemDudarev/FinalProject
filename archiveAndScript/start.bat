1baed08436bc774bc79a0e7bfadabd07738e3963
Artem <Artem.dudarev2003@gmail.com>
2025-03-24 14:53:00 +0300

    Fixed archive with .bat

--- archiveAndScript/start.bat
+++ archiveAndScript/start.bat
@@ -3,4 +3,6 @@
 set JAR_FILE=D:\1.SENLA_FINAL\FinalProject-0.0.1-SNAPSHOT.jar
 set JAVA_OPTS=-Xms512m -Xmx1024m
 
-java %JAVA_OPTS% -jar %JAR_FILE%
+"C:\Users\user\.jdks\openjdk-20.0.1\bin\java" %JAVA_OPTS% -jar %JAR_FILE%
+
+cmd /k
